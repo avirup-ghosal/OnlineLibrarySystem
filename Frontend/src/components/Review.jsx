90849737b58bc7f40e29c88b2a118dfea95b97ef
avirup-ghosal <avirupghosal2023@gmail.com>
2024-07-17 20:18:39 +0530

    add vercel link to review.jsx

--- Frontend/src/components/Review.jsx
+++ Frontend/src/components/Review.jsx
@@ -13,7 +13,7 @@ const saveReview = () => {
     const newEntry = { ...form, id: uuidv4() };
     setReview([...review, newEntry]);
 
-    axios.post('http://localhost:3001/review', newEntry)
+    axios.post('online-library-system-api-eight.vercel.app/review', newEntry)
       .then(response => {
         console.log('Review saved successfully:', response.data);
       })
